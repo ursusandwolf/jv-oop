57ab31bdd2eca90e46f7921b339aeee09b74f098
alex <ursusandwolf@protonmail.com>
2026-01-16 22:28:25 +0200

    fixed code style

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Truck working...");
+        System.out.println("Truck working!");
     }
 
     @Override
